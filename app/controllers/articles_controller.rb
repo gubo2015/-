48178c4b3b6060235dfc0b8946e3e563d8460c59
gubo2015 <356112797@qq.com>
2016-05-10 15:46:37 +0800

    安全认证

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+      http_basic_authenticate_with name: "1", password: "1", except: [:index, :show]
   def index
     @articles = Article.all
   end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+      http_basic_authenticate_with name: "2", password: "2", only: :destroy
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
