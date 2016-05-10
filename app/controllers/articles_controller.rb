de38120f39e1215ed4bb7b4f19db795fb9c77c84
gubo2015 <356112797@qq.com>
2016-05-10 13:38:47 +0800

    编辑更新文章和链接

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,15 @@ class ArticlesController < ApplicationController
   end
 
   def edit
+    @article = Article.find(params[:id])
+  end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
   private
     def article_params
